105ec85afca9c05307f77be61e0d8d8ff23227d1
Taheo <segieta.michal@gmail.com>
2018-04-06 13:22:30 +0200

    Bugfix in bundle for css site != Site

--- Diploma/App_Start/BundleConfig.cs
+++ Diploma/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Diploma
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                "~/Content/Site.css"));
     }
   }
 }
