e4f91d75b4bb630e5b50ef1023a0a901eec7f7bf
kastelikrobert <kastelikrobert@gmail.com>
2018-04-13 23:51:32 +0200

    AddEmailActivated migration

--- Diploma/Models/User.cs
+++ Diploma/Models/User.cs
@@ -8,13 +8,14 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Diploma.Models
 {
-  public class User
-  {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    [Key]
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public virtual List<Project> ListOfProjects { get; set; }
-  }
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Key]
+        public string Email { get; set; }
+        public bool EmailActivated { get; set; }
+        public string Password { get; set; }
+        public virtual List<Project> ListOfProjects { get; set; }
+    }
 }
