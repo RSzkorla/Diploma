5401fee77be2d42bcdac1f97d1230a84dcfc6dcf
RSzkorla <r.szkorla@gmail.com>
2018-04-30 17:32:53 +0200

    Interface IProjectService

---
+++ Diploma/BLL/IProjectService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diploma.Models;
+
+namespace Diploma.BLL
+{
+  interface IProjectService
+  {
+    void Create(Project project);
+    void Update(Project project);
+    void Delete(Project project);
+    void DeleteById(Guid id);
+    Project GetById(Guid id);
+    IEnumerable<Project> GetAll();
+    IEnumerable<Project> GetAllByUserId(Guid userGuid);
+  }
+}
