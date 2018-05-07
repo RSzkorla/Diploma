5e81028740a2e061d48bc190a6eb7b9152e0fd78
RSzkorla <r.szkorla@gmail.com>
2018-05-07 12:37:33 +0200

    ProjectTaskService <appSettings>    ...     <add key="owin:AutomaticAppStartup" value="false"/> </appSettings>

---
+++ Diploma/BLL/IProjectTaskService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diploma.Models;
+
+namespace Diploma.BLL
+{
+  interface IProjectTaskService
+  {
+    void Create(Guid projectId, ProjectTask newpProjectTask);
+    void Update(Guid projectId, Guid projectTaskId, ProjectTask updatedProjectTask);
+    void Delete(Guid projectId, Guid projectTaskId);
+    ProjectTask Get(Guid projectTaskId);
+    IEnumerable<ProjectTask> GetAllByProject(Guid projectId);
+  }
+}
---
+++ Diploma/BLL/ProjectTaskService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Diploma.Database;
+using Diploma.Models;
+
+namespace Diploma.BLL
+{
+  public class ProjectTaskService : IProjectTaskService
+  {
+    public void Create(Guid projectId, ProjectTask newpProjectTask)
+    {
+      using (var context = new DiplomaDBContext())
+      {
+        var project = context.ListOfProjects.SingleOrDefault(x => x.Id == projectId);
+        if (project != null)
+        {
+          project.ListOfProjectTasks.Add(newpProjectTask);
+        }
+        context.SaveChangesAsync();
+      }
+    }
+
+    public void Update(Guid projectId, Guid projectTaskId, ProjectTask updatedProjectTask)
+    {
+      using (var context = new DiplomaDBContext())
+      {
+        var project = context.ListOfProjects.SingleOrDefault(x => x.Id == projectId);
+        if (project != null)
+        {
+          var projectTask = project.ListOfProjectTasks.SingleOrDefault(x => x.Id == projectTaskId);
+          projectTask = updatedProjectTask;
+        }
+        context.SaveChangesAsync();
+      }
+    }
+
+    public void Delete(Guid projectId, Guid projectTaskId)
+    {
+      using (var context = new DiplomaDBContext())
+      {
+        var project = context.ListOfProjects.SingleOrDefault(x => x.Id == projectId);
+        if (project != null)
+        {
+          var projectTask = project.ListOfProjectTasks.SingleOrDefault(x => x.Id == projectTaskId);
+          project.ListOfProjectTasks.Remove(projectTask);
+        }
+        context.SaveChangesAsync();
+      }
+    }
+
+    public ProjectTask Get(Guid projectTaskId)
+    {
+      using (var context = new DiplomaDBContext())
+      {
+        return context.ListOfProjectTasks.SingleOrDefault(x => x.Id == projectTaskId);
+      }
+    }
+
+    public IEnumerable<ProjectTask> GetAllByProject(Guid projectId)
+    {
+      using (var context = new DiplomaDBContext())
+      {
+        var project = context.ListOfProjects.SingleOrDefault(x => x.Id == projectId);
+        if (project != null)
+          return project.ListOfProjectTasks;
+        return null;
+      }
+    }
+  }
+}
