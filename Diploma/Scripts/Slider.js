4475740764ac62828f734312653632d82f195a82
Taheo <segieta.michal@gmail.com>
2018-05-25 20:05:20 +0200

    slider ready to style, works

--- Diploma/Scripts/Slider.js
+++ Diploma/Scripts/Slider.js
@@ -1,95 +1,25 @@
 ﻿$(document).ready(function () {
-    //rotation speed and timer
-    //var speed = 115000;
+    // hide all quotes except the first
+    $('.slide').hide().eq(0).show();
 
-    //var run = setInterval(rotate, speed);
-    //var slides = $('.slide');
-    //var container = $('#slides ul');
-    //var elm = container.find(':first-child').prop("tagName");
-    //var item_width = container.width();
-    //var previous = 'prev'; //id of previous button
-    //var next = 'next'; //id of next button
-    //slides.width(item_width); //set the slides to the correct pixel width
-    //container.parent().width(item_width);
-    //container.width(slides.length * item_width); //set the slides container to the correct total width
-    //container.find(elm + ':first').before(container.find(elm + ':last'));
-    //resetSlides();
+    var pause = 3000;
+    var motion = 500;
 
+    var slide = $('.slide');
+    var count = slide.length;
+    var i = 0;
 
-    ////if user clicked on prev button
+    setTimeout(transition, pause);
 
-    //$('#buttons a').click(function (e) {
-    //    //slide the item
+    function transition() {
+        slide.eq(i).animate({ opacity: 'toggle', top: '40px' }, motion);
 
-    //    if (container.is(':animated')) {
-    //        return false;
-    //    }
-    //    if (e.target.id === previous) {
-    //        container.stop().animate({
-    //            'left': 0
-    //        }, 1500, function () {
-    //            container.find(elm + ':first').before(container.find(elm + ':last'));
-    //            resetSlides();
-    //        });
-    //    }
+        if (++i >= count) {
+            i = 0;
+        }
 
-    //    if (e.target.id === next) {
-    //        container.stop().animate({
-    //            'left': item_width * -2
-    //        }, 1500, function () {
-    //            container.find(elm + ':last').after(container.find(elm + ':first'));
-    //            resetSlides();
-    //        });
-    //    }
+        slide.eq(i).animate({ opacity: 'toggle', top: '20px' }, motion);
 
-    //    //cancel the link behavior            
-    //    return false;
-
-    //});
-
-    ////if mouse hover, pause the auto rotation, otherwise rotate it    
-    //container.parent().mouseenter(function () {
-    //    clearInterval(run);
-    //}).mouseleave(function () {
-    //    run = setInterval(rotate, speed);
-    //});
-
-
-    //function resetSlides() {
-    //    //and adjust the container so current is in the frame
-    //    container.css({
-    //        'left': -1 * item_width
-    //    });
-    //}
-
-    var animuspeed = 1000;
-    var speed = 5000;
-    var firstslide = $('.slide:first-of-type');
-    //var activeslide = $('.slide').hasClass("active-slide");
-   
-
-    var slidechildren = $('#slides').children();
-    let index = 0;
-
-
-    console.log(slidechildren);
-    $(document).ready(function () {
-        setInterval(function ()
-        {
-
-            for (var i = 0; i < 3; i++) {
-
-            }
-
-
-        }, speed);
-
-    });
-
+        setTimeout(transition, pause);
+    }
 });
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin
-
-function rotate() {
-    $('#next').click();
-}
