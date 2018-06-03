cd09024e45742797c04fd8e877fc835f93739ca5
kastelikrobert <kastelikrobert@gmail.com>
2018-06-03 12:20:47 +0200

    CreateTask view model

---
+++ Diploma/ViewModels/CreateTaskVM.cs
@@ -0,0 +1,21 @@
+﻿using Diploma.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Diploma.ViewModels
+{
+    public class CreateTaskVM
+    {
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DeadLine { get; set; }
+        public TaskTag Tag { get; set; }
+    }
+}
